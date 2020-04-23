c39cf07840fb00277db59fb96547d9f7004ddf7e
Bernardo Martinez <bjm72@duke.edu>
2020-04-22 17:59:02 -0700

    more plots and updated model

--- R/make_plots.R
+++ R/make_plots.R
@@ -22,12 +22,40 @@ grouped_date_data <- data %>%
 
 ggplot(data = grouped_date_data, mapping = aes(x = date, y = value)) + 
   geom_line() + 
-  facet_wrap(~ var, scales = 'free')+
-  theme_minimal()
+  facet_wrap(~ var, scales = 'free') + 
+  labs(y = "Average Value Each Day",
+       x = "Date",
+       title = "Trends In Each Audio Feature Over Time") + 
+  theme_minimal() +
+  theme(axis.line = element_line(color = 'black', linetype = 'solid'))
 
-grouped_position_data <- data %>% 
-  filter(!is.na(position_bracket)) %>% 
-  group_by(position_bracket, date) %>% 
-  select(c('date', 'position_bracket', "danceability","energy","key","loudness", "mode","speechiness","acousticness",
-           "instrumentalness","liveness","valence","tempo", 'duration_ms', "time_signature")) %>% 
-  summarise_all('mean')
+averaged <- data %>%
+  group_by(date) %>%
+  summarise(danceability = mean(danceability), energy = mean(energy), 
+            key = mean(key), loudness = mean(loudness), mode = mean(mode), 
+            speechiness = mean(speechiness), acousticness = mean(acousticness), 
+            instrumentalness = mean(instrumentalness), liveness = mean(instrumentalness), 
+            valence = mean(valence), tempo = mean(tempo), 
+            cases = mean(cases), deaths = mean(deaths))
+
+averaged %>%
+  dplyr::select(date, danceability, energy, key, speechiness, instrumentalness, liveness, valence, tempo, cases) %>%
+  gather(key = "var", value = "value", -date, -cases) %>%
+  ggplot(aes(x = value, y = log(cases))) +
+  geom_point() +
+  facet_wrap(~ var, scales = "free") +
+  theme_minimal() + 
+  labs(title = "Number of Average Cases Across Different Values of Audio Features",
+       x = "Values",
+       y = "Log of Cases Per Day") + 
+  theme(axis.line = element_line(color = 'black', linetype = 'solid'))
+
+
+
+
+
+
+
+
+
+
